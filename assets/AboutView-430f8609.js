a60092825316afcd0fa04376e269ea51e5510b58
kevincolompar <146346889+kevincolompar@users.noreply.github.com>
2023-09-28 16:28:18 +0200

    Add files via upload

---
+++ assets/AboutView-430f8609.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as s}from"./index-18167f00.js";const _={},c={class:"about"},n=s("h1",null,"This is a page on my work",-1),a=[n];function r(i,d){return o(),t("div",c,a)}const u=e(_,[["render",r]]);export{u as default};
